ec5b6862f08e02fbf7ee5b90553925af3edf9232
JaeWooYi <wodn0101@gmail.com>
2022-09-05 18:29:02 +0900

    Typhoon

--- grap-market-web/src/App.js
+++ grap-market-web/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ChildComponent from "./JWtest/child1.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <div>
+      <h1>abcd Test</h1>
+      <ChildComponent />
+      <ChildComponent />
+      <ChildComponent />
     </div>
   );
 }
---
+++ grap-market-web/src/JWtest/child1.js
@@ -0,0 +1,5 @@
+function ChildComponent() {
+  return <p>I'm child</p>;
+}
+
+export default ChildComponent;
