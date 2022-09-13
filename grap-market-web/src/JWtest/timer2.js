abd9979e117c063966b125e22e398ae4ca53e093
JaeWooYi <wodn0101@gmail.com>
2022-09-13 13:42:34 +0900

    I'm ediot...

--- grap-market-web/src/JWtest/timer2.js
+++ grap-market-web/src/JWtest/timer2.js
@@ -3,13 +3,19 @@ import React from "react";
 function TimerComponent() {
   const [time, setTime] = React.useState(0);
   console.log("Component Update");
-  function updateTime() {
-    setTime(time + 1); // 클릭할때마다 1씩 증가
-  }
+  // function updateTime() {
+  //   setTime(time + 1); // 클릭할때마다 1씩 증가
+  // }
+  React.useEffect(function () {
+    //   setTime(time + 1);
+    // }, [time]); // 무한으로 됨
+    setTime(time + 1);
+  }, []);
   return (
     <div>
       <h3>{time}+</h3>
-      <button onClick={updateTime}>increase 1</button>
+      {/* <button onClick={updateTime}>increase 1</button> */}
+      <button>increase 1</button>
     </div>
   );
 }
