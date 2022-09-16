0c0ba7b39d32567964e06bd784c01ae8882046bb
JaeWooYi <wodn0101@gmail.com>
2022-09-16 20:45:13 +0900

    뭔말이야....

--- grap-market-web/src/main/index.js
+++ grap-market-web/src/main/index.js
@@ -1,6 +1,24 @@
 import "./index.css"; // 현재 있는 위치에서(index.js안에있으니까), 또 css같은경우 따로 from을 적지 않는다.
+import axious from "axios";
+import React from "react";
 
 function MainPage() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axious
+      .get(
+        "https://4dfcb0c6-21f6-45ec-b9c9-2c959574210f.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        console.log("RESULT!! ---> " + result);
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.log("ERROR!!!! ---> " + error);
+      });
+  }, []);
+
   return (
     <div>
       <div id="header">
@@ -13,7 +31,30 @@ function MainPage() {
           <img src="images/banners/banner1.png" />
         </div>
         <h1>Products Sold</h1>
-        <div id="product-list"></div>
+        <div id="product-list">
+          {products.map(function (product, index) {
+            return (
+              <div classNaem="product-card">
+                <div>
+                  <img classNaem="product-img" src={product.imageUrl} />
+                </div>
+                <div>
+                  <div classNaem="product-contents">
+                    <span classNaem="product-name">{product.name}</span>
+                    <span classNaem="product-price">{product.price}</span>
+                    <div classNaem="product-seller">
+                      <img
+                        classNaem="product-avatar"
+                        src="images/icons/avatar.png"
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div id="footer"></div>
     </div>
