fd3e5ef333deed0c504bc981fe8a07aad291e89a
JaeWooYi <wodn0101@gmail.com>
2022-09-18 12:26:37 +0900

    Sunday!! very difficult!!

--- grap-market-web/src/App.js
+++ grap-market-web/src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
 import MainPageComponent from "./main/index.js";
 // import MainPageComponent from "./main" // 생략가능
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload/index.js";
+import ProdcutPage from "./product/index.js";
 
 function App() {
-  return <MainPageComponent></MainPageComponent>;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path="/product">
+          <ProdcutPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ grap-market-web/src/product/index.js
@@ -0,0 +1,5 @@
+function ProdcutPage() {
+  return <h1>Prodcut Page!!</h1>;
+}
+
+export default ProdcutPage;
