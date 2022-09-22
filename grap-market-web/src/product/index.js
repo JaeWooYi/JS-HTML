345e0c2c6c7f52be5e2277af7e7c4040f19f739c
JaeWooYi <wodn0101@gmail.com>
2022-09-22 20:29:47 +0900

    Thursday!!

--- grap-market-web/src/product/index.js
+++ grap-market-web/src/product/index.js
@@ -1,7 +1,25 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function ProductPage() {
   const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://4dfcb0c6-21f6-45ec-b9c9-2c959574210f.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(product);
+
   return <h1>Prodcut Page {id} Product!!</h1>;
 }
 
