241637056a2a139a14c5ce6f39f9f1c1b1808221
JaeWooYi <wodn0101@gmail.com>
2022-09-25 12:46:50 +0900

    React is difficult....

--- grap-market-web/src/App.js
+++ grap-market-web/src/App.js
@@ -1,24 +1,48 @@
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import "./App.css";
 import MainPageComponent from "./main/index.js";
 // import MainPageComponent from "./main" // 생략가능
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link, useHistory } from "react-router-dom";
 import UploadPage from "./upload/index.js";
 import ProdcutPage from "./product/index.js";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 function App() {
+  const history = useHistory();
   return (
     <div>
-      <Switch>
-        <Route exact={true} path="/">
-          <MainPageComponent />
-        </Route>
-        <Route exact={true} path="/products/:id">
-          <ProdcutPage />
-        </Route>
-        <Route exact={true} path="/upload">
-          <UploadPage />
-        </Route>
-      </Switch>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            {/* "/" 기본 경로로 가겠다. */}
+            <img src="/images/icons/logo.png" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function () {
+              history.push("/upload");
+            }}
+            icon={<DownloadOutlined />}
+          >
+            Product Upload!!
+          </Button>
+        </div>
+      </div>
+      <div id="body">
+        <Switch>
+          <Route exact={true} path="/">
+            <MainPageComponent />
+          </Route>
+          <Route exact={true} path="/products/:id">
+            <ProdcutPage />
+          </Route>
+          <Route exact={true} path="/upload">
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
+      <div id="footer"></div>
     </div>
   );
 }
--- grap-market-web/src/product/index.js
+++ grap-market-web/src/product/index.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
   const { id } = useParams();
@@ -18,9 +19,27 @@ function ProductPage() {
       });
   }, []);
 
-  console.log(product);
+  if (product == null) {
+    return <h1>Getting Prodcut Information....</h1>;
+  }
 
-  return <h1>Prodcut Page {id} Product!!</h1>;
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}$</div>
+        <div id="createdAt">2022.09.25</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
